6d088c9dd76e22d92fe3d2149293f89411a13a09
SurajDeepmindz <suraj@pharynxai.com>
2026-07-08 15:18:26 +0530

    Project Initialize..

---
+++ src/test/java/com/example/AIRBNBCLONE/AirbnbcloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AIRBNBCLONE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirbnbcloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
